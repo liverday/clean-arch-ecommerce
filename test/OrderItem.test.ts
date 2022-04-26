4e803962604a9ce95083b4062e9ada366b4a035b
liverday <vitor.medeiro10@gmail.com>
2022-04-25 22:35:33 -0300

    fix(test): fix OrderItem test

--- test/OrderItem.test.ts
+++ test/OrderItem.test.ts
@@ -3,7 +3,7 @@ import OrderItem from "../src/OrderItem";
 
 test('should be able to create an order item', () => {
   const orderItem = new OrderItem(new Item(1, 'Guitarra', 500), 10);
-  expect(orderItem.getTotal()).toBe(500);
+  expect(orderItem.getTotal()).toBe(5000);
   expect(orderItem.item.idItem).toBe(1);
   expect(orderItem.item.description).toBe('Guitarra');
   expect(orderItem.item.price).toBe(500);
