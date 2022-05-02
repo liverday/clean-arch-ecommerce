b53ce660e91bd45c1221b87fc176afa38600d05a
liverday <vitor.medeiro10@gmail.com>
2022-05-02 19:12:59 -0300

    refactor: change descriptions

--- test/Coupon.test.ts
+++ test/Coupon.test.ts
@@ -8,10 +8,10 @@ test('should create a coupon', () => {
 
 test('should not create a coupon that is expired', () => {
   const coupon = new Coupon('EXPIRADO10', 10, subDays(new Date(), 1))
-  expect(coupon.isExpired()).toBe(true) 
+  expect(coupon.isExpired(new Date())).toBe(true) 
 })
 
-test('a coupo that expire date was not defined should return true to isExpired', () => {
+test('a coupon that expire date was not defined should return false to isExpired', () => {
   const coupon = new Coupon('VALE10', 10);
   expect(coupon.isExpired()).toBe(false);
 })
